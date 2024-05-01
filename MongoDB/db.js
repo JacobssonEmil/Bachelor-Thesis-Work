6de690a2173c8aea1fff413437586390c0bb8fe9
gnagetEmil <emil@gnaget.se>
2024-05-01 20:49:12 +0200

    removed seperate db file for connecting

--- MongoDB/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/performanceTest')
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-module.exports = mongoose;
